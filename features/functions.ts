ea78b5d5f5a065470da5a2989668604fad88bf59
skywindzz <skywindzz@yahoo.com>
2022-10-26 21:46:46 -0700

    typed arrary lesson

--- features/functions.ts
+++ features/functions.ts
@@ -5,3 +5,63 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+const subtract = (a: number, b: number): number => {
+  return a - b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+//annotation for anaomous functionannotation for arrow function
+const multiply = (a: number, b: number): number => {
+  return a * b;
+};
+
+
+//Void and Never;  void means this function doesn't return anything, never is when a function will never 
+const logger = (message: string): void => {
+  console.log(message);
+};  
+ 
+const throwError = (message: string): never => { 
+  throw new Error(message);
+})
+
+//destructuring with annotations
+const logWeather = {
+  date: new Date();
+  weather: 'sunny';
+}
+
+const logWeather = (forecast: { date: Date, weather: string }): void => {
+  console.log(forecast.date);
+  console.log(forecast.weather);
+}
+
+//To destructure and pull out the value in a function
+
+const logWeather = ({ date, weather }: { date: Date, weather: string }) => { 
+  console.log(date);
+  console.log(weather);
+}
+
+//Annotation in objects
+
+const profile = {
+  name: alex;
+  age: 20;
+  coords: {
+    lat: 0,
+    lng: 15
+  },
+  setAge(age: number): void {
+    this.age = age;
+   }
+}
+
+//notice when you do destrucuring, you have to specify the property seperately instead of just doing { age: number }
+const { age }: { age: number } = profile;
+
+const { coords: { lat, lng } }: { coords: { lat:number, lng:number} } = profile;
